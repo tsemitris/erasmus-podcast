a355931e3f8b5503dee7b9be7278cb73377bba27
Nikolaos Sarris <nikossarris29@gmail.com>
2025-05-16 20:12:54 +0200

    Add global scss file

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -16,7 +16,7 @@ export const Home = () => {
         loading="lazy"
       />
 
-      <div className="home-main-container">
+      <div className="home-main-container main-container">
         <div className="home-main-inner-container">
           <div className="home-title-container">
             <img
