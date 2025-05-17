39da2d470888f2b724241cab16e8f29d2c8982fa
Nikolaos Sarris <94851943+tsemitris@users.noreply.github.com>
2025-05-17 19:33:49 +0200

    Add logotype

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,9 +1,23 @@
+import { useNavigate } from "react-router-dom";
 import "./_header.scss";
 
 export const Header = () => {
+  const navigate = useNavigate();
+
   return (
     <>
-      <header className="main-header"></header>
+      <header className="home-header">
+        <div className="header-inner-container max-width">
+          <div className="header-logotype" onClick={() => navigate("/")}>
+            <img
+              className="website-logotype"
+              src="./logo/light/erasmus-podcast.png"
+              alt=""
+            />
+            <p>Erasmus Podcast</p>
+          </div>
+        </div>
+      </header>
     </>
   );
 };
